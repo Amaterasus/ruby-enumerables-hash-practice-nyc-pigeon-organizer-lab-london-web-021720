ce59f8123699967386e962d04d90af8d42627e98
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 09:17:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   output = {}
   
-  data.each do |attribute|
+  data.each do |attribute, attribute_array|
     attribute.each do |nested_attribute , names|
       names.each do |name|
         output[name] = {attribute => []}
