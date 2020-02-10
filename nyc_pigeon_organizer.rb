3ac1fc2f2086afb3f24e684ea74210060442211c
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 09:23:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  output = {}
+  
+  data.each do |attribute, attribute_array|
+    attribute_array.each do |nested_attribute , names|
+      names.each do |name|
+        if !output[name]
+          output[name] = {attribute => []}
+        end
+        
+        if !output[name][attribute]
+          output[name][attribute] = []
+        end
+        
+        output[name][attribute].push(nested_attribute.to_s)
+      end
+    end
+  end
+  
+  return output
 end
